1050148a7b1fc248f50abaf124a19a01b4150467
Ikki <123062700+Ikkiiiii@users.noreply.github.com>
2023-01-19 09:36:30 +0100

    Initiële upload van de code.
    hehe

---
+++ VoorbeeldConsolee/VoorbeeldConsolee/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoorbeeldConsolee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YOOOO");
+            Console.ReadKey();
+        }
+    }
+}
